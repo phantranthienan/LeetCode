c1403b5aad62cae17ca63f52ee01ac993eead042
Phan Tran Thien An <phantranthienan1405@gmail.com>
2024-02-01 10:25:23 +0100

    add solution for graph problems

---
+++ Problems&Solutions/DSA/visiblePeopleInQueue.py
@@ -0,0 +1,22 @@
+# 1944. Number of Visible People in a Queue
+# There are n people standing in a queue, and they numbered from 0 to n - 1 
+# in left to right order. You are given an array heights of distinct integers 
+# where heights[i] represents the height of the ith person.
+
+# A person can see another person to their right in the queue if everybody 
+# in between is shorter than both of them. More formally, the ith person can see the jth person 
+# if i < j and min(heights[i], heights[j]) > max(heights[i+1], heights[i+2], ..., heights[j-1]).
+
+# Return an array answer of length n where answer[i] is the 
+# number of people the ith person can see to their right in the queue.
+
+def canSeePersonsCount(heights):
+    res = [0] * len(heights)
+    stack = []
+    for i in range(len(heights)):
+        while stack and heights[i] > heights[stack[-1]]:
+            res[stack.pop()] += 1
+        if stack:
+            res[stack[-1]] += 1
+        stack.append(i)
+    return res
